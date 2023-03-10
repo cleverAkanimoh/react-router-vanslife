984752bbb402eae0747bf062bb9786eb5d703c2a
cleverAkanimoh <crushclever1@gmail.com>
2023-03-10 16:44:14 +0100

    modified 3 files

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,10 +1,14 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 
-export default function Vans() {
+export default function () {
 
     const [vansData, setVansData] = useState([])
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    const typeFilter = searchParams.get("type")
+    console.log(typeFilter);
 
     useEffect(() => {
         fetch("/api/vans")
@@ -12,7 +16,11 @@ export default function Vans() {
             .then(data => setVansData(data.vans))
     }, [])
 
-    const vanElements = vansData.map(van => (
+    const displayedVans = typeFilter
+        ? vansData.filter(van => van.type.toLowerCase() === typeFilter)
+        : vansData
+
+    const vanElements = displayedVans.map(van => (
         <div key={van.id} className="van-tile">
             <Link to={`/vans/${van.id}`} >
                 <img src={van.imageUrl} />
@@ -25,15 +33,27 @@ export default function Vans() {
         </div>
     ))
 
-    const vanButtons = vansData.map(van => (
-        <button key={van.id} className="van-btn">{van.type}</button>
-    ))
-
     return (
         <div className="van-list-container">
             <h2>Explore our vans options</h2>
-            <div className="btnContainer">
-                {vanButtons}
+            <div className="van-list-filter-buttons">
+                <Link
+                    to="?type=simple"
+                    className="van-type simple"
+                >Simple</Link>
+                <Link
+                    to="?type=luxury"
+                    className="van-type luxury"
+                >Luxury</Link>
+                <Link
+                    to="?type=rugged"
+                    className="van-type rugged"
+                >Rugged</Link>
+                <Link
+                    to="."
+                    className="van-type clear-filters"
+                >Clear filter</Link>
+
             </div>
             <div className="van-list">
                 {vanElements}
